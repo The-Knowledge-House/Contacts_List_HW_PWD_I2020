8ec1b0a8bfa9f3285054eddd796e9b92ba2a665b
Shafee <shafeelinks@gmail.com>
2021-01-08 19:20:10 -0500

    contact list first completion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contacts from "./Components/Contacts";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Contact List</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Contact List</h1>
+			<Contacts />
+		</div>
+	);
 }
 
 export default App;
