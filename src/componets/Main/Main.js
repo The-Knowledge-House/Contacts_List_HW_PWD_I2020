37023a0142629eb0b62d865fc23a74563131a28a
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-10 21:24:19 -0500

    update css

--- src/componets/Main/Main.js
+++ src/componets/Main/Main.js
@@ -3,16 +3,18 @@ import "./Main.css";
 const Main = ({ name, picture, cell, phone }) => {
   return (
     <div className="container">
-      <img src={picture.large} alt={name.first} />
+      <div className="img-div">
+        <img src={picture.large} alt={name.first} />
+        <div className="num">
+          <p>Home:{phone}</p>
+          <p> Phone:{cell}</p>
+        </div>
+      </div>
 
       <div className="contactInfo">
-        <h3>
+        <h4>
           {name.first} {name.last}
-        </h3>
-
-        <p>Home:{phone}</p>
-
-        <p> Phone:{cell}</p>
+        </h4>
       </div>
     </div>
   );
