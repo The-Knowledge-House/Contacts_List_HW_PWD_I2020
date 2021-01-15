67719fc44f7be3a5649334444b7e11b1ed58cf8a
zeita101 <zeitajb1@gmail.com>
2021-01-14 23:42:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contacts from "./components/Contacts";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Contact List</h1>
+      <Contacts />
+      {/* <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className="user-details">
+        <p>Name: Jenny Han</p>
+        <p>Phone: (55) 555-5555</p>
+        <p>Cell: (55) 555-5555</p>
+      </div> */}
     </div>
   );
 }
