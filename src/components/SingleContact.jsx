9b19ad79f0c3218f60ea409c14103c72a9b95a88
Leesel Fraser <leeselfraser08@gmail.com>
2021-01-15 20:11:57 -0500

    style

--- src/components/SingleContact.jsx
+++ src/components/SingleContact.jsx
@@ -1,13 +1,13 @@
 const SingleContact = (props) => {
   const { picture, name, phone, cell } = props.contact;
   return (
-    <div>
+    <div class="card">
       <img src={picture.thumbnail} alt="contact headshot" />
-      <h3>
+      <h3 class="side">
         {name.first} {name.last}
       </h3>
-      <h5>Home: {phone}</h5>
-      <h5>Mobile: {cell}</h5>
+      <h5 class="side">Home: {phone}</h5>
+      <h5 class="side">Mobile: {cell}</h5>
     </div>
   );
 };
