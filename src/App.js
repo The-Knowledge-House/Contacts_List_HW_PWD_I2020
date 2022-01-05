887a64ab087d950e82afd8834df824af0b3e7032
Hydeia <blakeyhydeia@gmail.com>
2022-01-05 12:41:57 -0500

    finish homework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import PhoneBook from './components/presentational/PhoneBook.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Contact List</h1>
+      <PhoneBook />
+      
     </div>
   );
 }
