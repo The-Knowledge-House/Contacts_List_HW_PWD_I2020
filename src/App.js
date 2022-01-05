aeee985ae87e24bac70ccf39ce6e5df7f2c945f4
Jas <c.jasminher@gmail.com>
2022-01-05 15:24:45 -0500

    Finished contact list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ContactCard from "./components/ContactCard";
+import data from "./data/contacts.json";
 
 function App() {
+  const list = data.results.map((item) => (
+    <ContactCard
+      image={item.picture.large}
+      name={item.name.first + " " + item.name.last}
+      home={item.phone}
+      mobile={item.cell}
+    />
+  ));
+
   return (
     <div className="App">
       <h1>Contact List</h1>
+    <div className="container">
+
+      {list}
+    </div>
     </div>
   );
 }
